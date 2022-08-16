4874fcacee8c1d0fe88f08a2b207e1477e412de6
fellypebardales <86852698+fellypebardales@users.noreply.github.com>
2022-08-16 17:53:38 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const botaoMenu = document.querySelector(".header_menu")
-const menu = document.querySelector(".nav_menu_list")
-
-botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('nav_menu_list-at')
-})
-
-
